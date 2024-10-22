f6670e535de07d854a5fd73da55958beb4ace722
HS-hyeokjin <youngurwls@gmail.com>
2024-10-22 20:10:41 +0900

    fix : baseEntity 수정
    - 초기값 설정

--- Backend/edututor/src/main/java/com/tls/edututor/common/entity/BaseEntity.java
+++ Backend/edututor/src/main/java/com/tls/edututor/common/entity/BaseEntity.java
@@ -3,6 +3,8 @@ package com.tls.edututor.common.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
@@ -13,13 +15,15 @@ import java.time.LocalDateTime;
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
 
+  @CreatedDate
   @Column(name = "CREATED_AT", nullable = false, updatable = false)
   private LocalDateTime createdAt;
 
+  @LastModifiedDate
   @Column(name = "UPDATED_AT")
   private LocalDateTime updatedAt;
 
-  @Column(name = "WRITER", nullable = false)
+  @Column(name = "WRITER")
   private Long writer;
 
   @Column(name = "UPDATER")
@@ -27,5 +31,12 @@ public abstract class BaseEntity {
 
   @Column(name = "IS_DELETED", nullable = false)
   private Boolean isDeleted;
+
+  @PrePersist
+  protected void onCreate() {
+    if (this.isDeleted == null) {
+      this.isDeleted = false;
+    }
+  }
 
 }
