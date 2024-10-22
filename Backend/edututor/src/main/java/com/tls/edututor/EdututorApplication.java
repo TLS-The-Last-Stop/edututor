88ba720cc1fba0247712e32da9a25b0978638fb6
HS-hyeokjin <youngurwls@gmail.com>
2024-10-22 10:12:51 +0900

    init : 프로젝트 초기화

---
+++ Backend/edututor/src/main/java/com/tls/edututor/EdututorApplication.java
@@ -0,0 +1,13 @@
+package com.tls.edututor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdututorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdututorApplication.class, args);
+	}
+
+}
---
+++ Backend/edututor/src/test/java/com/tls/edututor/EdututorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tls.edututor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdututorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
