5dfd0ddb93d098dde6a6734556665812105e107c
HS-hyeokjin <youngurwls@gmail.com>
2024-10-23 21:12:00 +0900

    feat : 예외처리 코드 추가

--- Backend/edututor/src/main/java/com/tls/edututor/common/exception/GlobalExceptionHandler.java
+++ Backend/edututor/src/main/java/com/tls/edututor/common/exception/GlobalExceptionHandler.java
@@ -4,13 +4,25 @@ import com.tls.edututor.common.api.CommonApiResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
   @ExceptionHandler(Exception.class)
-  public ResponseEntity<CommonApiResponse<Void>> handleGenericException(Exception ex) {
+  public ResponseEntity<CommonApiResponse<Void>> handleGenericException() {
     return ResponseEntity.status(CommonApiResponse.INTERNAL_SERVER_ERROR)
             .body(CommonApiResponse.createInternalServerError("서버 오류"));
+  }
+
+  @ExceptionHandler(NoResourceFoundException.class)
+  public CommonApiResponse<Void> handleNoResourceFoundException(NoResourceFoundException ex, WebRequest request) {
+    return CommonApiResponse.createNotFound("༼ つ ◕_◕ ༽つ 요청경로가 이상함 여기 아닌듯 : " + request.getDescription(false));
+  }
+
+  @ExceptionHandler(IllegalArgumentException.class)
+  public CommonApiResponse<Void> handleBadRequestException(IllegalArgumentException ex) {
+    return CommonApiResponse.createBadRequest("(┬┬﹏┬┬) 서버에서 뭔가 잘못됨 ㅅㄱ    " + ex.getMessage());
   }
 }
