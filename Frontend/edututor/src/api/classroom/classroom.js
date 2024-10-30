ae591cdb31af9310742672b7420532f3dae4230f
dduwan <leesuwan89@gmail.com>
2024-10-30 09:37:40 +0900

    fix: classroom.js 수정

--- Frontend/edututor/src/api/classroom/classroom.js
+++ Frontend/edututor/src/api/classroom/classroom.js
@@ -1,9 +1,6 @@
-import axios from 'axios';
-
-axios.defaults.withCredentials = true;
-const BASE_URL = import.meta.env.VITE_BASE_URL;
+import privateApi from '../axios.js';
 
 export const getAllStudent = async (classroomId) => {
-  const response = await axios.get(`${BASE_URL}/classrooms/${classroomId}/students`);
+  const response = await privateApi.get(`/classrooms/${classroomId}/students`);
   return response.data;
 };
