383747c6c145dbbb9c16e2905a02d48b9053c022
dduwan <leesuwan89@gmail.com>
2024-11-01 11:39:53 +0900

    fix: 지니아튜터 pdf 파일

--- Frontend/edututor/src/components/classroom/StudentList.jsx
+++ Frontend/edututor/src/components/classroom/StudentList.jsx
@@ -7,6 +7,11 @@ const StudentCard = styled.div`
     height: 115px;
     box-shadow: 1px 1px 3px 3px rgba(0, 0, 0, 0.1);
     margin-bottom: 1rem; // 카드 사이 간격
+    cursor: pointer;
+
+    &:hover {
+        box-shadow: 0px 4px 12px rgba(0, 0, 0, 0.1);
+    }
 
     &:last-child {
         margin-bottom: 0; // 마지막 카드는 마진 제거
