b177be386a596184d823a39e1c5a97bf91806aba
dduwan <leesuwan89@gmail.com>
2024-11-07 23:40:06 +0900

    fix: AuditorAwareImpl oauth 로그인 관련 수정

--- Backend/edututor/src/main/java/com/tls/edututor/common/aop/AuditorAwareImpl.java
+++ Backend/edututor/src/main/java/com/tls/edututor/common/aop/AuditorAwareImpl.java
@@ -4,6 +4,7 @@ import com.tls.edututor.user.dto.response.AuthUser;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 
 import java.util.Optional;
 
@@ -17,7 +18,16 @@ public class AuditorAwareImpl implements AuditorAware<Long> {
       return Optional.empty();
     }
 
-    AuthUser authUser = (AuthUser) authentication.getPrincipal();
-    return Optional.of(authUser.getId());
+    Object principal = authentication.getPrincipal();
+
+    if (principal instanceof AuthUser) {
+      return Optional.of(((AuthUser) principal).getId());
+    } else if (principal instanceof DefaultOAuth2User) {
+      DefaultOAuth2User oAuth2User = (DefaultOAuth2User) principal;
+
+      return Optional.of(oAuth2User.getAttribute("id"));
+    }
+
+    return Optional.empty();
   }
 }
